db286dad358774b4d9a3e8215a796f3f3d3fc550
batanoffs <batanoff.s@protonmail.com>
2024-10-25 16:21:15 +0300

    add hook useForm to handle the form and import it in the component

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,89 +1,31 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
 import { CustomForm, CustomInput } from "../../components/index";
-import data from "../../users.json";
+import { useForm } from "../../hooks/useForm";
 
 import styles from "./login.module.css";
 
-//TODO implement responsive design to Login if needed
 export const LoginPage = () => {
-    const [formData, setFormData] = useState({
+    const { values, changeHandler, onSubmitHandler } = useForm({
         user: "",
         password: "",
     });
-    const navigate = useNavigate();
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-        const formElement = e.target;
-        const form = new FormData(formElement);
-
-        if (!form) throw new Error("Form data is empty");
-
-        const formEntries = Object.fromEntries(form);
-
-        const { user, password } = formEntries;
-
-        const isValidUser = data.users.find((u) => u.user === user);
-
-        if (!isValidUser) {
-            alert("User does not exist");
-            return;
-        }
-
-        if (isValidUser.password !== password) {
-            alert("Wrong password");
-            return;
-        }
-
-        navigate("/table");
-    };
-
-    const handleEmptyFields = () => {
-        const button = document.querySelector(".login-button");
-
-        if (formData.user !== "" && formData.password !== "") {
-            button.disabled = false;
-        } else {
-            button.disabled = true;
-        }
-    };
-
-    const onChangeHandler = (e) => {
-        const target = e.currentTarget.name;
-        const value = e.currentTarget.value;
-
-        if (!target) throw new Error("target is null");
-
-        setFormData((prev) => {
-            if (target === "user") {
-                return { ...prev, user: value };
-            } else {
-                return { ...prev, password: value };
-            }
-        });
-
-        handleEmptyFields();
-    };
 
     return (
         //TODO improve design
         <div className={styles.container}>
             <h1>Login</h1>
-            <CustomForm submitHandler={submitHandler}>
+            <CustomForm submitHandler={onSubmitHandler}>
                 <CustomInput
                     id="user"
                     name="user"
-                    value={formData.user}
-                    onChange={onChangeHandler}
+                    value={values.user}
+                    onChange={changeHandler}
                 />
                 <CustomInput
                     id="password"
                     name="password"
                     type="password"
-                    value={formData.password}
-                    onChange={onChangeHandler}
+                    value={values.password}
+                    onChange={changeHandler}
                 />
                 <CustomInput
                     className="login-button"
