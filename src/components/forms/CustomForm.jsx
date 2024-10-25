fa4ebec61c28148e5bea05a863157165c988a7ab
batanoffs <batanoff.s@protonmail.com>
2024-10-25 15:45:09 +0300

    remove button html from component

--- src/components/forms/CustomForm.jsx
+++ src/components/forms/CustomForm.jsx
@@ -1,11 +1,3 @@
 export const CustomForm = ({ children, submitHandler }) => {
-    return (
-        <form onSubmit={submitHandler}>
-            {children}
-
-            <button className="login-button" disabled type="submit">
-                Login
-            </button>
-        </form>
-    );
+    return <form onSubmit={submitHandler}>{children}</form>;
 };
