03ff5210a368b9d7db4c80abcd256e1bbd89eba3
batanoffs <batanoff.s@protonmail.com>
2024-10-25 15:45:22 +0300

    add check for type submit

--- src/components/forms/CustomInput.jsx
+++ src/components/forms/CustomInput.jsx
@@ -3,11 +3,23 @@ import { formatString } from "../../utils/index";
 export const CustomInput = ({
     type,
     placeholder,
+    disabled = false,
     name,
     value,
     onChange,
     className,
 }) => {
+    if (type === "submit") {
+        return (
+            <input
+                type={type}
+                value={value}
+                className={className}
+                disabled={disabled}
+            />
+        );
+    }
+
     return (
         <div>
             <label htmlFor={name}>
