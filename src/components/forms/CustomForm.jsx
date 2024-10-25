3225e1b29347bf4d9e73f82eb28a804bc0c8ff7a
batanoffs <batanoff.s@protonmail.com>
2024-10-25 14:42:57 +0300

    clean up

--- src/components/forms/CustomForm.jsx
+++ src/components/forms/CustomForm.jsx
@@ -9,28 +9,3 @@ export const CustomForm = ({ children, submitHandler }) => {
         </form>
     );
 };
-
-/* <form onSubmit={submitHandler}>
-    <div>
-        <label htmlFor="user"> User </label>
-        <input
-            id="user"
-            name="user"
-            value={formData.user}
-            onChange={onChangeHandler}
-        />
-    </div>
-    <div>
-        <label htmlFor="password"> Password </label>
-        <input
-            id="password"
-            name="password"
-            type="password"
-            value={formData.password}
-            onChange={onChangeHandler}
-        />
-    </div>
-    <button className="login-button" disabled type="submit">
-        Login
-    </button>
-</form> */
