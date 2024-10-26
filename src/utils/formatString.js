2d9fe35ea0cedaf9c3e4d256b2b498469d1b48ce
batanoffs <batanoff.s@protonmail.com>
2024-10-26 14:33:53 +0300

    update extension and add types

--- src/utils/formatString.js
+++
@@ -1,7 +0,0 @@
-const firstCharUpperCase = (name) => {
-    return name.charAt(0).toUpperCase() + name.slice(1);
-};
-
-export const formatString = {
-    firstCharUpperCase,
-};
---
+++ src/utils/formatString.ts
@@ -0,0 +1,7 @@
+const firstCharUpperCase = (name: string) => {
+	return name.charAt(0).toUpperCase() + name.slice(1);
+};
+
+export const formatString = {
+	firstCharUpperCase,
+};
