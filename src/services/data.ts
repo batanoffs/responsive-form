54403da4f9c500db2b74cd01ca1961de591b0c00
batanoffs <batanoff.s@protonmail.com>
2024-10-26 14:32:18 +0300

    add interface and update data types with imported interface, update extension

---
+++ src/services/data.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { Person } from '../interfaces/person.interface';
+
+const getPeople = async (url: string) => {
+	try {
+		const response = await axios.get(url);
+		const filterProps = response.data.results.map((person: Person) => ({
+			name: person.name,
+			mass: person.mass,
+			height: person.height,
+			hair_color: person.hair_color,
+			skin_color: person.skin_color,
+		}));
+
+		const pagination = {
+			next: response.data.next,
+			prev: response.data.previous,
+		};
+
+		return { filterProps, pagination };
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const dataService = {
+	getPeople,
+};
